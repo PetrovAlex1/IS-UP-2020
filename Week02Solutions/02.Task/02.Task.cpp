eff5c76f44c0ae76688d93dbbfaa1a1993010190
AlexDays <AlexDays@students.softuni.bg>
2020-10-27 20:01:54 +0200

    Add "02-task-w02" project

--- Week02Solutions/02.Task/02.Task.cpp
+++ Week02Solutions/02.Task/02.Task.cpp
@@ -1,20 +1,22 @@
-// 02.Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char letter;
+	std::cin >> letter;
+
+	if ((int)letter > 97 && (int)letter < 122)
+	{
+		letter = (char)(letter - 32);
+		std::cout << "The upper case character corresponding to is " << letter;
+	}
+	else if ((int)letter > 65 && (int)letter < 90)
+	{
+		letter = (char)(letter + 32);
+		std::cout << "The lower case character corresponding to is " << letter;
+	}
+	else
+	{
+		std::cout << "is not a letter";
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
