7bf6f7c7d69d1ed05054c56fcafdc6a9fbe77fce
AlexDays <AlexDays@students.softuni.bg>
2020-10-27 20:02:51 +0200

    Add "03-task-w02" project

--- Week02Solutions/03.Task/03.Task.cpp
+++ Week02Solutions/03.Task/03.Task.cpp
@@ -1,20 +1,32 @@
-// 03.Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int month;
+	std::cin >> month;
+
+	switch (month)
+	{
+	case 12:
+	case 1:
+	case 2:
+		std::cout << "The season is winter!";
+		break;
+	case 3:
+	case 4:
+	case 5:
+		std::cout << "The season is spring!";
+		break;
+	case 6:
+	case 7:
+	case 8:
+		std::cout << "The season is summer!";
+		break;
+	case 9:
+	case 10:
+	case 11:
+		std::cout << "The season is autumn!";
+	default:
+		break;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
