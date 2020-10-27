741b12986a20640f4d083baae88399d0641407b8
AlexDays <AlexDays@students.softuni.bg>
2020-10-27 20:04:56 +0200

    Add "05-task-w02" project

--- Week02Solutions/05.Task/05.Task.cpp
+++ Week02Solutions/05.Task/05.Task.cpp
@@ -1,20 +1,47 @@
-// 05.Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int month, year;
+	std::cin >> month >> year;
+	int thirdDigit = (year % 100) / 10;
+	int fourthDigit = year % 1000;
+
+	switch (month)
+	{
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		std::cout << 31;
+		break;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		std::cout << 30;
+		break;
+	case 2:
+		if (thirdDigit == 0 && fourthDigit == 0)
+		{
+			if (year % 400 == 0)
+			{
+				std::cout << 29;
+			}
+		}
+		else if (year % 4 == 0)
+		{
+			std::cout << 29;
+		}
+		else
+		{
+			std::cout << 28;
+		}
+		break;
+	default:
+		break;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
