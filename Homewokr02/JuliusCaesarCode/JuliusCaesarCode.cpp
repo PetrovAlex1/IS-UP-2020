8e15660afd5934ee05bee1b25df06678997f89ed
AlexDays <AlexDays@students.softuni.bg>
2020-12-30 22:13:48 +0200

    Add "JuliusCaesarCode" project

--- Homewokr02/JuliusCaesarCode/JuliusCaesarCode.cpp
+++ Homewokr02/JuliusCaesarCode/JuliusCaesarCode.cpp
@@ -1,20 +1,52 @@
-// JuliusCaesarCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+char* Decryption(char* code, int countElements, int n)
+{
+	for (int i = 0; i < countElements; i++)
+	{
+		if (((int)code[i] - n) >= 65 && (((int)code[i] - n) <= 90))
+		{
+			code[i] = (char)((int)code[i] - n);
+		}
+		else
+		{
+			code[i] = (char)((int)code[i] + 25 - n);
+		}
+	}
+
+	return code;
+}
+
+int CountElements(char code[])
+{
+	int i = 0, counter = 0;
+
+	while ((int)code[i] >= 65 && (int)code[i] <= 90)
+	{
+		i++;
+		counter++;
+	}
+
+	return counter;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char* code = new char[64];
+	int n, i = 0;
+
+	std::cin.getline(code, 64);
+	std::cin >> n;
+
+	int countElements = CountElements(code);
+
+	code = Decryption(code, countElements, n);
+
+	for (int i = 0; i < countElements; i++)
+	{
+		std::cout << code[i];
+	}
+
+	delete[] code;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
