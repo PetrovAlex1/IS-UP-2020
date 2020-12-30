fc754f73bc2615fda1e9e7154308bca115e60bf8
AlexDays <AlexDays@students.softuni.bg>
2020-12-30 21:24:04 +0200

    Add "SecondaryDiagonal" project

--- Homewokr02/SecondaryDiagonal/SecondaryDiagonal.cpp
+++ Homewokr02/SecondaryDiagonal/SecondaryDiagonal.cpp
@@ -1,20 +1,68 @@
-// SecondaryDiagonal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+int** FillInMatrix(int** matrix, int n)
+{
+	int countNullsInRow = n - 1;
+	for (int i = 0; i < n; i++)
+	{
+		int j = 0;
+
+		for (j = 0; j < countNullsInRow; j++)
+		{
+			matrix[i][j] = 0;
+		}
+
+		countNullsInRow--;
+
+		matrix[i][j] = 1;
+
+		for (int l = j + 1; l < n; l++)
+		{
+			matrix[i][l] = 2;
+		}
+	}
+
+	return matrix;
+}
+
+int** CreateMatrix(int n)
+{
+	int** matrix = 0;
+	matrix = new int* [n];
+
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = new int[n];
+
+		for (int j = 0; j < n; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+
+	return matrix;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+
+	std::cin >> n;
+
+	int** matrix = CreateMatrix(n);
+
+
+	matrix = FillInMatrix(matrix, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			std::cout << matrix[i][j] << " ";
+		}
+
+		std::cout << std::endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
