1b8cbdfdd5d8f10d652f7011a72ab9511911f685
AlexDays <AlexDays@students.softuni.bg>
2020-12-30 19:16:07 +0200

    Add "Recursion" project

--- Homewokr02/Recursion/Recursion.cpp
+++ Homewokr02/Recursion/Recursion.cpp
@@ -1,20 +1,94 @@
-// Recursion.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+#define cout(x) std::cout << x << std::endl
+#define cin(x) std::cin >> x
+
+char* ReplaceWithAsterisk(char* arr, char symbol)
+{
+	int i = 0;
+
+	while (arr[i] != '\0')
+	{
+		if (arr[i] == symbol)
+		{
+			arr[i] = '*';
+		}
+
+		i++;
+	}
+
+	return arr;
+}
+
+bool CheckAsterisk(char arr[])
+{
+	int i = 0;
+
+	while (arr[i] != '\0')
+	{
+		if (arr[i] != '*')
+		{
+			return false;
+		}
+
+		i++;
+	}
+
+	return true;
+}
+
+
+//This funcion will find if all elements on the first text contain on the second one. When it's find a common elemen, this element will
+//be replaced with asterisk in the first texts, even if it occurs more than once.
+bool CompareSymbols(char firstText[], char* secondText, int i)
+{
+	int j = 0;
+	int count = 0;
+	bool exist = false;
+
+	if (firstText[i] == '*')
+	{
+		i++;
+		return CompareSymbols(firstText, secondText, i++);
+	}
+
+	while (secondText[j] != '\0')
+	{
+		if (secondText[j] != '*')
+		{
+			if (firstText[i] == secondText[j])
+			{
+				firstText = ReplaceWithAsterisk(firstText, firstText[i]);
+				i++;
+				return CompareSymbols(firstText, secondText, i++);
+			}
+		}
+		j++;
+	}
+
+	if (CheckAsterisk(firstText))
+	{
+		return true;
+	}
+
+	return false;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char firstText[64];
+	cin(firstText);
+	char secondText[64]{ '*' };
+	cin(secondText);
+
+	if (CompareSymbols(firstText, secondText, 0))
+	{
+		cout("YES");
+	}
+	else
+	{
+		cout("NO");
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
