d83bb87e65fee41652a0b77a9624ddd243c3a7cd
AlexDays <AlexDays@students.softuni.bg>
2021-01-03 18:43:57 +0200

    Add "CountDigits" project

--- Homewokr02/CountDigits/CountDigits.cpp
+++ Homewokr02/CountDigits/CountDigits.cpp
@@ -1,20 +1,33 @@
-// CountDigits.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+int CountDigits(char text[], int counter)
+{
+	int i = 0;
+
+	while (text[i] != '\0')
+	{
+		if ((int)text[i] >= 48 && (int)text[i] <= 57)
+		{
+			counter++;
+			text[i] = '*';
+			return CountDigits(text, counter);
+		}
+
+		i++;
+	}
+
+	return counter;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char text[64];
+
+	std::cin.getline(text, 64);
+	
+	int countDigits = CountDigits(text, 0);
+
+	std::cout << countDigits;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
