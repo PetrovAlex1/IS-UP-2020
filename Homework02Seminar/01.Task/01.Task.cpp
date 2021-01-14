3a3e80a287093e5256c8fe4b82b92ac8b2107eeb
AlexDays <AlexDays@students.softuni.bg>
2021-01-15 00:14:35 +0200

    Add "01.Task" project

--- Homework02Seminar/01.Task/01.Task.cpp
+++ Homework02Seminar/01.Task/01.Task.cpp
@@ -1,20 +1,107 @@
-// 01.Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+void ToLower(char* text)
+{
+	if (text[0] == '\0')
+	{
+		return;
+	}
+	if (text[0] >= 'A' && text[0] <= 'Z')
+	{
+		text[0] += 32;
+	}
+
+	return ToLower(text + 1);
+
+}
+
+int CountLetters(char* text)
+{
+	int counter = 0;
+
+	while (text[counter] != '\0')
+	{
+		counter++;
+	}
+
+	return counter;
+}
+
+bool CheckPalindromeRecursion(char* text, int countSymbols, int counter)
+{
+	static int half = countSymbols / 2;
+
+	if (counter == half)
+	{
+		return true;
+	}
+	else if (text[0] == ' ')
+	{
+		return CheckPalindromeRecursion(text + 1, --countSymbols, counter);
+	}
+	else if (text[countSymbols] == ' ')
+	{
+		return CheckPalindromeRecursion(text, --countSymbols, counter);
+	}
+	else if (text[0] == text[countSymbols])
+	{
+		countSymbols -= 2;
+		counter++;
+		return CheckPalindromeRecursion(text + 1, countSymbols, counter);
+	}
+	else
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool CheckPalindrome(char* text, int countSymbols)
+{
+	int index = countSymbols - 1;
+
+	for (int i = 0; i < countSymbols / 2; i++)
+	{
+		if (text[i] == ' ')
+		{
+			continue;
+		}
+		else if (text[index] == ' ')
+		{
+			index--;
+			i--;
+			continue;
+		}
+		else if (text[i] != text[index])
+		{
+			return false;
+		}
+
+		index--;
+	}
+
+	return true;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char* palindorme = new char[128];
+	int i = 0;
+
+	std::cin.getline(palindorme, 128);
+
+	ToLower(palindorme);
+
+	int countSymbols = CountLetters(palindorme);
+	bool checkPalindromR = CheckPalindromeRecursion(palindorme, countSymbols - 1, 0);
+	bool checkPalindrom = CheckPalindrome(palindorme, countSymbols);
+
+	std::cout << "Result with recursion : ";
+	std::cout << std::boolalpha << checkPalindromR << std::endl;
+	std::cout << "Result without recursion : ";
+	std::cout << std::boolalpha << checkPalindrom << std::endl;
+
+	delete[] palindorme;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
