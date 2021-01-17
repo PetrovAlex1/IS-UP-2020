11bbc1e01515c7fc8e66b8c8ad96b8bc22f291b0
AlexDays <AlexDays@students.softuni.bg>
2021-01-17 22:50:03 +0200

    Add "03.Task" project

--- Homework02Seminar/03.Task/03.Task.cpp
+++ Homework02Seminar/03.Task/03.Task.cpp
@@ -1,20 +1,153 @@
-// 03.Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+struct Player
+{
+private:
+	char* name;
+	int health;
+	int strength;
+	int level;
+	int bicepsSize;
+public:
+	char* GetName()
+	{
+		return name;
+	}
+
+	int GetHealth()
+	{
+		return health;
+	}
+
+	int GetStrength()
+	{
+		return strength;
+	}
+
+	int GetLevel()
+	{
+		return level;
+	}
+
+	int GetBicepsSize()
+	{
+		return bicepsSize;
+	}
+
+	void SetName(char* _name)
+	{
+		name = _name;
+	}
+
+	void SetHealth(int _health)
+	{
+		health = _health;
+	}
+
+	void SetStrength(int _strength)
+	{
+		strength = _strength;
+	}
+
+	void SetLevel(int _level)
+	{
+		level = _level;
+	}
+
+	void SetBicepsSize(int _bicepsSize)
+	{
+		bicepsSize = _bicepsSize;
+	}
+
+	void TakeDamake(int damage)
+	{
+		health -= damage;
+	}
+};
+
+void Fight(Player firstPlayer, Player secondPlayer, int firstPunch)
+{
+	int order = firstPunch;
+
+	while (true)
+	{
+		int damage = 0;
+
+		if (order % 2 == 0)//is second player's move
+		{
+			damage = secondPlayer.GetLevel() * secondPlayer.GetBicepsSize() + secondPlayer.GetStrength();
+			firstPlayer.TakeDamake(damage);
+			order++;
+		}
+		else//is first player's move
+		{
+			damage = firstPlayer.GetLevel() * firstPlayer.GetBicepsSize() + firstPlayer.GetStrength();
+			secondPlayer.TakeDamake(damage);
+			order++;
+		}
+		if (firstPlayer.GetHealth() < 0 || secondPlayer.GetHealth() < 0)
+		{
+			break;
+		}
+	}
+
+	if (order % 2 != 0)
+	{
+		std::cout << "The fight won -> " << secondPlayer.GetName() << " with " << secondPlayer.GetHealth() << " health points left.";
+	}
+	else
+	{
+		std::cout << "The fight won -> " << firstPlayer.GetName() << " with " << firstPlayer.GetHealth() << " health points left.";
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Player firstPlayer, secondPlayer;
+	int currentHealth = 0, currentStrength = 0, currentLevel = 0, currentBicepsSize = 0;
+	char* currentName = new char[8];
+
+	//first player input
+	std::cout << "Enter first player's name: ";
+	std::cin >> currentName;
+	firstPlayer.SetName(currentName);
+	std::cout << "Enter first player's health points: ";
+	std::cin >> currentHealth;
+	firstPlayer.SetHealth(currentHealth);
+	std::cout << "Enter first player's strength: ";
+	std::cin >> currentStrength;
+	firstPlayer.SetStrength(currentStrength);
+	std::cout << "Enter first player's level: ";
+	std::cin >> currentLevel;
+	firstPlayer.SetLevel(currentLevel);
+	std::cout << "Enter first player's biceps size: ";
+	std::cin >> currentBicepsSize;
+	firstPlayer.SetBicepsSize(currentBicepsSize);
+
+
+	//second player input
+	std::cout << "Enter second player's name: ";
+	std::cin >> currentName;
+	secondPlayer.SetName(currentName);
+	std::cout << "Enter second player's health points: ";
+	std::cin >> currentHealth;
+	secondPlayer.SetHealth(currentHealth);
+	std::cout << "Enter second player's strength: ";
+	std::cin >> currentStrength;
+	secondPlayer.SetStrength(currentStrength);
+	std::cout << "Enter second player's level: ";
+	std::cin >> currentLevel;
+	secondPlayer.SetLevel(currentLevel);
+	std::cout << "Enter second player's biceps size: ";
+	std::cin >> currentBicepsSize;
+	secondPlayer.SetBicepsSize(currentBicepsSize);
+
+	int firstPunch = 0;
+	std::cout << "Enter which will be the first punch. Enter 1 for the first player and 2 for the second: ";
+	std::cin >> firstPunch;
+
+
+	Fight(firstPlayer, secondPlayer, firstPunch);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
